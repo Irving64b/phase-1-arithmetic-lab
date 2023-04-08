a70d8fda99e34616cde062cb64effcf3995a3cf0
Irving Medina <irving.64@hotmail.com>
2023-04-08 13:42:20 -0400

    complete arithmetic edit

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ let num1 = 2;
 let num2 = 31;
 let multiply = num1*num2;
 
-let random = 1
-Math.floor(Math.random() * 10) + 1;
+let random = Math.floor(Math.random() * 10) + 1;
 
 let num3 = 16
 let num4 = 6
 let mod = num3%num4;
 
-let max = 20;
-Math.max()
+let max = Math.max(5,10,15,20);
